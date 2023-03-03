85205cf763622b1292968f5343c1ccc80602b97d
Marcos Vinícius <69282670+marcondesu@users.noreply.github.com>
2023-03-03 05:56:04 -0300

    upload: recursividade e ranqueamento com critérios

--- Buscador/main.py
+++ Buscador/main.py
@@ -1,7 +1,10 @@
+import warnings
 import requests
 import requests_cache
 from bs4 import BeautifulSoup
+from urllib3.exceptions import InsecureRequestWarning
 
+warnings.filterwarnings('ignore', category=InsecureRequestWarning)
 requests_cache.install_cache('banco')
 
 def mostrar_dicionario_ordenado(dicionario):
@@ -9,65 +12,86 @@ def mostrar_dicionario_ordenado(dicionario):
 
     index = 1
     for chave, valor in sorted(dicionario.items(), key=lambda item: item[1], reverse=True):
-        print(f"{index}º: '{chave}' -> {valor}")
+        print(f"{index}º: '{chave}' -> {valor:.0f}")
         
         index += 1
 
-def busca(termo, url, profundidade):
-    rank = {url: 0} # dicionário com os seguintes campos: {site: quantidade_de_ocorrencias}
-    resposta = requests.get(url, verify=False)
-    soup = BeautifulSoup(resposta.content, 'html.parser')
-    texto = soup.get_text()
+rank = {} # dicionário com os seguintes campos: {site: quantidade_de_ocorrencias}
+ocorrencias = []
+qtd_ocorrencias = 0
 
+def verificar_ocorrencias(texto, termo, url):
     posicao = texto.find(termo)
+    rank[url] += 1 # critério positivo de referência
+    rank[url] -= len(url) / 10 # critério negativo de tamanho da url
+
+    if posicao != -1: rank[url] = 0 # caso encontre pelo menos uma ocorrência, adicione no rank
 
     while posicao != -1:
         inicio = max(0, posicao - 20)
         fim = min(len(texto), posicao + len(termo) + 20)
-        print(f'## {texto[inicio:fim]}')
 
-        rank[url] += 1 # incrementa a quantidade de ocorrências
+        global qtd_ocorrencias
+        qtd_ocorrencias += 1
+        if texto[inicio:fim].replace('\n', '') not in ocorrencias:
+            ocorrencias.append(texto[inicio:fim].replace('\n', '')) # guarda o trecho da ocorrência removendo quebras de linha
+
+        rank[url] += 1 # critério positivo de ocorrências encontradas na página
         posicao = texto.find(termo, posicao + 1)
+
+def busca(termo, url, profundidade):
+    rank[url] = 0 # adiciona a página no ranqueamento
+    resposta = requests.get(url, allow_redirects=True, verify=False) # 200 -> sucesso
+    print(f'> {url}')
+
+    soup = BeautifulSoup(resposta.content, 'html.parser')
+    texto = soup.get_text()
+    verificar_ocorrencias(texto, termo, url)
     
-    while profundidade > 0:
-        html = BeautifulSoup(resposta.text, 'html5lib')
-        links = html.find_all('a', href=True)
+    if profundidade > 0:
+        resposta = BeautifulSoup(resposta.text, 'html5lib')
+        links = resposta.find_all('a', href=True)
 
         # itera em todos os links da página
         for link in links:
+            # tratamento no link para transformar '/t/music/' -> 'https://reddit.com/t/music'
             link = link['href']
-                
-            if (link[:1] == '/'):
+            if link[:4] != 'http':
                 link = url + link
 
-            # request da página
-            response = requests.get(link, verify=False)
-            root = BeautifulSoup(response.content, 'html.parser')
-            texto = root.get_text()
-            posicao = texto.find(termo)
+            if link in rank:
+                continue
 
-            if posicao != -1: rank[link] = 0 # caso encontre pelo menos uma ocorrência, adicione no rank
-
-            # printando todas ocorrências
-            while posicao != -1:
-                inicio = max(0, posicao - 20)
-                fim = min(len(texto), posicao + len(termo) + 20)
-                print(f'## {texto[inicio:fim]}'.replace('\n', '')) # remove quebras de linha e printa a ocorrência
-
-                rank[link] += 1 # incrementa a quantidade de ocorrências
-                posicao = texto.find(termo, posicao + 1)
-        
-        profundidade -= 1
-
-    mostrar_dicionario_ordenado(rank)
+            busca(termo, link, profundidade - 1)
 
 def main():  
-    url = 'https://www.reddit.com'
+    url = input('URL: ')
     termo = input('Termo a ser buscado: ')
     profundidade = int(input('Profundidade: '))
     print()
 
     busca(termo, url, profundidade)
+
+    menu = '\n-------------- MENU --------------\n\n'
+    menu += '1. Mostrar ocorrências\n'
+    menu += '2. Mostrar rank\n'
+    menu += '\n0. Sair\n'
+
+    print(menu)
+    opcao = int(input('> '))
+    
+    while opcao != 0:
+        if opcao == 1:
+            for ocorrencia in ocorrencias:
+                print(ocorrencia)
+
+            print(f"\n - Total de {len(ocorrencias)} ocorrências.")
+
+        elif opcao == 2:
+            mostrar_dicionario_ordenado(rank)
+
+        print(menu)
+        opcao = int(input('> '))
 
 if __name__ == '__main__':
     main()
