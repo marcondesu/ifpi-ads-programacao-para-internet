87491a8972f29b51c168ca4a041865068d0742ef
Marcos Vinícius <rkatchau@gmail.com>
2023-03-17 14:00:52 -0300

    update(Servidor/Cliente): permanencia de partidas

--- Jogo-em-Rede/src/servidor.ts
+++ Jogo-em-Rede/src/servidor.ts
@@ -1,8 +1,10 @@
 import * as net from 'net'
+import * as fs from 'fs'
 
 // array que armazena as conexões
 let sockets: net.Socket[] = []
-let scoreboard: number[] = []
+let matches: number[] = []
+let counter = 1 // quantidade de partidas/rodadas
 let value_1: number, value_2: number, result: number
 
 // gera novos valores aleatórios para cada rodada
@@ -19,38 +21,87 @@ const message_all = (message: string): void => {
    })
 }
 
+const get_matchesStr = (): string => {
+   let matchesStr: string = ''
+
+   sockets.forEach((player) => {
+      if (player == sockets[sockets.length - 1]) {
+         matchesStr += `${player.remotePort}-${matches[sockets.indexOf(player)]}`
+      } else {
+         matchesStr += `${player.remotePort}-${matches[sockets.indexOf(player)]}-`
+      }
+   })
+
+   return matchesStr
+}
+
+const get_matchesInfo = (): string => {
+   let info = ''
+   let lines = fs.readFileSync('./matches.txt').toString().split('\n')
+   let i = 0
+   
+   while (lines[i] != null) {
+      const line = lines[i].split('-')
+      info += `\nPartida ${i + 1}\n (${line[0]}): ${line[1]}\n (${line[2]}): ${line[3]}\n`
+
+      i++
+   }
+
+   return info
+}
+
 const server: net.Server = net.createServer((socket: net.Socket) => {
    console.log(`$ Cliente conectado: ${socket.remoteAddress}:${socket.remotePort}`)
    sockets.push(socket)
-   scoreboard.push(0)
+   matches.push(0)
 
-   if (sockets.length === 2) { // jogo inicia quando houver 2 clientes conectados
-      message_all('Jogo iniciado.')
+   if (sockets.length >= 2) { // jogo inicia quando houver 2 clientes conectados
+      message_all(`Partida ${counter}`)
 
       nova_jogada()
       message_all(` ${value_1} + ${value_2} = ?`) // envia os operandos da nova rodada para todos os jogadores
    }
    
    socket.on('data', (data: Buffer) => { // função que trata os dados enviados pelo cliente ao servidor
+      if (data.toString() === '/partidas') { // se o jogador digitar '/partidas'
+         socket.write(get_matchesInfo()) // envia uma string contendo as informações das partidas
+      }
+
       if (data.toString() === `${result}`) { // se o jogador digitou o valor do resultado, venceu a rodada
-         scoreboard[sockets.indexOf(socket)]++ // atualiza o array de pontuações na posição do vencedor
+         matches[sockets.indexOf(socket)]++ // atualiza o array de pontuações na posição do vencedor
          
          console.log(`$ (${socket.remotePort}) Pontuou`)
 
-         for (let value of scoreboard) { // percorre o array de pontuações
+         for (let value of matches) { // percorre o array de pontuações
             if (value === 2) { // se o valor da posição corrente for igual a 2, o cliente na mesma posição no array de sockets é o vencedor
-               message_all(`\n$ Jogador ${sockets[scoreboard.indexOf(2)].remotePort} venceu!`)
+               message_all(`\n$ Jogador ${sockets[matches.indexOf(2)].remotePort} venceu!`)
+               let matches_string = get_matchesStr()
+
+               try{
+                  let file = fs.readFileSync('./matches.txt', 'utf-8') // conteúdo do arquivo .txt
+                  
+                  if (file.charAt(file.length - 1) !== '\n') { // se o último caractere do arquivo não for uma quebra de linha, adiciona
+                     file += '\n'
+                  }
+
+                  fs.writeFileSync('./matches.txt', file + matches_string) // atualiza os placares
+               } catch (error) {
+                  console.error(`Erro: ${error}`)
+               }
 
                sockets.forEach((player) => {
                   player.end() // desconecta todos jogadores
                })
+
+               matches_string = ''
+               matches = [] // esvazia vetor do placar
 
                return // return para não ser gerada uma nova rodada
             }
          }
 
          nova_jogada()
-         message_all(`\nNova rodada\n ${value_1} + ${value_2} = ?`)
+         message_all(`\Partida ${counter} \n ${value_1} + ${value_2} = ?`)
       }
    })
 
